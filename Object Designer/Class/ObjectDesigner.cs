b477bd3271c142a2c878ed2a9384c50d8f0cbf1d
Mateus Ribeiro Culpo <mateusculpo@gmail.com>
2016-04-08 10:57:23 -0300

    Editor Designer
    Criar opções de cenário , e configurar a cena... depois escolher uma
    cena autoconfigurada.

---
+++ Object Designer/Class/ObjectDesigner.cs
@@ -0,0 +1,151 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using LitJson;
+
+public class ObjectDesigner : MonoBehaviour
+{
+
+    private const string PATH = "/Object Designer/Activity Resource/{0}.txt";
+
+    public string m_nameActivity = "none";
+    public int m_amountObject;
+
+    public TextAsset m_assetActivity;
+    public Transform[] m_object;
+    public List<ResourceObjectDesigner> m_activityDesigner;
+
+
+    public bool loadResourceActivity(TextAsset _assetText)
+    {
+        try
+        {
+            string _text = _assetText.text;
+
+            if (_assetText == null)
+            {
+                ///Busca o caminho do Json no Editor
+                string path = string.Format(Application.dataPath + PATH, m_nameActivity);
+                //Usa o FILE para buscar a informação dentro do caminhos passado por parametro
+                _text = File.ReadAllText(path);
+            }
+
+            ///Transforma a informação do json e alimenta a lista de teclados virtuais configurados
+            if (_text != null)
+                this.m_activityDesigner = new List<ResourceObjectDesigner>(JsonMapper.ToObject<List<ResourceObjectDesigner>>(_text));
+
+
+            /// Load objects ref
+            this.m_amountObject = this.m_activityDesigner[0].m_structObjectDesigner.Count;
+            this.m_nameActivity = _assetText.name;
+            this.m_object = new Transform[this.m_amountObject];
+
+            for (int i = 0; i < this.m_amountObject; i++)
+            {
+                
+                GameObject obj = GameObject.Find(this.m_activityDesigner[0].m_structObjectDesigner[i].m_nameRef).gameObject;
+                print(obj.name);
+                this.m_object[i] = obj.transform;
+            }
+
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    public void saveActivityObjectDesigner()
+    {
+        if (m_activityDesigner == null)
+            m_activityDesigner = new List<ResourceObjectDesigner>();
+
+
+        ResourceObjectDesigner resource = new ResourceObjectDesigner();
+        resource.m_structObjectDesigner = new List<StructObjectDesigner>();
+
+        for (int i = 0; i < m_object.Length; i++)
+        {
+            StructObjectDesigner _object = new StructObjectDesigner();
+            if (m_object[i] != null)
+            {
+                _object.m_nameRef = m_object[i].name;
+                _object.m_idRef = m_object[i].GetInstanceID();
+                _object.m_activity = m_object[i].gameObject.activeSelf;
+                _object.m_positionRef = m_object[i].gameObject.transform.position;
+                _object.m_scaleRef = m_object[i].gameObject.transform.localScale;
+                _object.m_rotationRef = new Vector3(m_object[i].gameObject.transform.rotation.x, m_object[i].gameObject.transform.rotation.y, m_object[i].gameObject.transform.rotation.z);
+            }
+            resource.m_structObjectDesigner.Add(_object);
+        }
+
+        m_activityDesigner.Add(resource);
+    }
+
+    public void deleteObjectDesigner(int _indexObject)
+    {
+        m_activityDesigner.RemoveAt(_indexObject);
+    }
+
+    public bool createFile()
+    {
+#if UNITY_EDITOR
+        string path = string.Format(Application.dataPath + PATH, m_nameActivity);
+
+        if (!File.Exists(path))
+            File.Create(path);
+        else
+            return false;
+
+        return true;
+#endif
+    }
+
+    public bool saveFileActivity()
+    {
+#if UNITY_EDITOR
+
+        string path = string.Format(Application.dataPath + "/Object Designer/Activity Resource/{0}.txt", m_nameActivity);
+        if (File.Exists(path))
+        {
+            try
+            {
+                File.WriteAllText(path, JsonMapper.ToJson(m_activityDesigner));
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        else
+            return false;
+        return true;
+#endif
+    }
+}
+
+
+public class activityObjectDesigner
+{
+    public string m_titleRef;
+    public bool m_toolBarObjectAcitivity = true;
+    public List<ResourceObjectDesigner> m_resource;
+}
+
+public class ResourceObjectDesigner
+{
+    public List<StructObjectDesigner> m_structObjectDesigner { set; get; }
+}
+
+public class StructObjectDesigner
+{
+    public string m_nameRef { set; get; }
+    public int m_idRef { set; get; }
+    public bool m_activity { set; get; }
+    public Vector3 m_positionRef { set; get; }
+    public Vector3 m_rotationRef { set; get; }
+    public Vector3 m_scaleRef { set; get; }
+}
---
+++ Object Designer/Editor/ObjectDesignerEditor.cs
@@ -0,0 +1,200 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+
+[CustomEditor(typeof(ObjectDesigner))]
+public class ObjectDesignerEditor : Editor
+{
+    //#if UNITY_EDITOR
+    string CurrentStringKeyBoard;
+
+    int CurrentCountObj;
+
+    Transform transform;
+
+    bool toolBarActivity = true;
+    bool toolBarObjectAcitivity = true;
+
+    public override void OnInspectorGUI()
+    {
+        ObjectDesigner script = (ObjectDesigner)target;
+
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.LabelField("Nome da atividade");
+        script.m_nameActivity = EditorGUILayout.TextField(script.m_nameActivity);
+        EditorGUILayout.EndHorizontal();
+
+        Space(1);
+
+        EditorGUILayout.HelpBox("Estes objetos nao podem ser salvos inativos dentro da cena", MessageType.Warning);
+
+        EditorGUILayout.BeginHorizontal();
+
+        if (GUILayout.Button("Build"))
+        {
+            script.m_object = new Transform[(int)script.m_amountObject];
+        }
+
+        if (GUILayout.Button("Clear"))
+        {
+            script.m_object = new Transform[0];
+        }
+
+        EditorGUILayout.LabelField("Quantidade");
+        script.m_amountObject = EditorGUILayout.IntField(script.m_amountObject);
+
+        EditorGUILayout.EndHorizontal();
+
+
+        int countObject = 0;
+
+        if (script.m_object != null)
+        {
+            foreach (Transform obj in script.m_object)
+            {
+                EditorGUILayout.BeginHorizontal();
+
+                script.m_object[countObject] = EditorGUILayout.ObjectField(script.m_object[countObject], typeof(Transform), true) as Transform;
+
+                EditorGUILayout.EndHorizontal();
+
+                countObject++;
+            }
+
+        }
+
+        Space(2);
+
+        EditorGUILayout.BeginHorizontal();
+
+        if (GUILayout.Button("Load Arquivo"))
+        {
+            bool loadFile;
+
+            if (script.m_assetActivity != null)
+                loadFile = script.loadResourceActivity(script.m_assetActivity);
+            else
+                loadFile = script.loadResourceActivity(null);
+
+
+            if (loadFile)
+                EditorUtility.DisplayDialog("Carregado", "Arquivo Carregado com Sucesso", "Prosseguir");
+            else
+            {
+                EditorUtility.DisplayDialog("Carregado", "Arquivo nao encontrado", "Prosseguir");
+            }
+        }
+
+        script.m_assetActivity = EditorGUILayout.ObjectField(script.m_assetActivity, typeof(TextAsset), false) as TextAsset;
+
+        EditorGUILayout.EndHorizontal();
+
+        Space(1);
+
+        if (GUILayout.Button("Criar Arquivo"))
+        {
+            bool createFile = script.createFile();
+            if (createFile)
+                EditorUtility.DisplayDialog("Arquivo", "Arquivo Criado com Sucesso", "Prosseguir");
+            else
+            {
+                EditorUtility.DisplayDialog("Arquivo", "Arquivo Ja existe", "Prosseguir");
+            }
+        }
+
+        Space(1);
+
+        if (GUILayout.Button("Salvar Arquivo"))
+        {
+            bool booleanSaveFile;
+            booleanSaveFile = script.saveFileActivity();
+            if (booleanSaveFile)
+                EditorUtility.DisplayDialog("Arquivo", "Arquivo Salvado com Sucesso", "Prosseguir");
+            else
+            {
+                if (EditorUtility.DisplayDialog("Arquivo", "Nao foi possivel salvar o arquivo, primeiro deve criar um arquivo, deseja criar o arquivo", "Sim", "Nao"))
+                {
+                    bool createFile = script.createFile();
+                    if (createFile)
+                        EditorUtility.DisplayDialog("Arquivo", "Arquivo Criado com Sucesso", "Prosseguir");
+                }
+            }
+        }
+
+        Space(1);
+
+        EditorGUILayout.BeginHorizontal();
+
+        if (GUILayout.Button("Criar um Object de Designer"))
+        {
+            script.saveActivityObjectDesigner();
+        }
+
+        if (GUILayout.Button("Limpar Lista"))
+        {
+            if (EditorUtility.DisplayDialog("Deletar", "Deseja Limpar a lista de Objetos", "Sim", "Nao"))
+            {
+                script.m_activityDesigner = null;
+            }
+        }
+
+        EditorGUILayout.EndHorizontal();
+        Space(1);
+
+        if (script.m_activityDesigner != null)
+        {
+            toolBarActivity = EditorGUILayout.Foldout(toolBarActivity, "Activity Designer");
+
+            if (toolBarActivity)
+            {
+                if (script.m_activityDesigner != null)
+                {
+                    int countObjectPerActivity = -1;
+                    foreach (ResourceObjectDesigner activityObjectDesigner in script.m_activityDesigner)
+                    {
+                        countObjectPerActivity++;
+                        EditorGUILayout.BeginHorizontal();
+
+                        EditorGUILayout.LabelField(script.m_nameActivity + " " + countObjectPerActivity.ToString());
+
+                        if (GUILayout.Button("Delete Objeto"))
+                        {
+                            if (EditorUtility.DisplayDialog("Deletar Objeto",
+                                                "Deseja deletar este Objeto?"
+                                                , "Sim", "Nao Desejo"))
+                            {
+                                script.deleteObjectDesigner(countObjectPerActivity);
+                                if (script.m_activityDesigner.Count == 0) script.m_activityDesigner = null;
+                                return;
+                            }
+                        }
+
+                        if (GUILayout.Button("Save Designer Atual"))
+                        {
+                            if (EditorUtility.DisplayDialog("Atualizar Objeto",
+                                                "Deseja atualizar este Objeto?"
+                                                , "Sim", "Nao Desejo"))
+                            { }
+                            return;
+                        }
+                        EditorGUILayout.EndHorizontal();
+                    }
+                }
+            }
+        }
+    }
+
+    void OnInspectorUpdate()
+    {
+        Repaint();
+    }
+
+    void Space(int _quantidade)
+    {
+        for (int i = 0; i < _quantidade; i++)
+        {
+            EditorGUILayout.Space();
+        }
+    }
+}
